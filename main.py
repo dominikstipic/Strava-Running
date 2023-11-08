efcd32c18a813c38b5d3ef32cf2de272323cb15c
dominikstipic <dominikstipic@gmail.com>
2023-11-08 11:45:43 +0100

    export to excel

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ data = strava.process()
 df = pd.DataFrame(data)
 
 DF = df[['name', 'distance', 'moving_time', 'elapsed_time','total_elevation_gain', 
-'type', 'sport_type', 'start_date_local', 'location_city', 'location_state', 
-'location_country','map', 'trainer', 'start_latlng', 'end_latlng',
+'type', 'sport_type', 'start_date_local','map', 'trainer', 'start_latlng', 'end_latlng',
 'average_speed', 'max_speed', 'elev_high', 'elev_low']]
 
 DF = DF.sort_values("start_date_local")
-DF.to_csv("data.csv")
+DF["start_date_local"] = DF["start_date_local"].apply(lambda x:x.strftime("%d-%m-%Y %H:%M"))
+DF.to_excel("data.xlsx")
